634c168d66bc089acc3908decfb530dcf0227906
atsb <44937323+atsb@users.noreply.github.com>
2024-08-27 20:57:27 +0200

    Version bump and using DXVK for d3d9

--- src/version.h
+++ src/version.h
@@ -41,12 +41,12 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "3.1.0"
+#define VERSIONSTR "3.2.0"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 3,1,0,0
-#define RC_PRODUCTVERSION 3,1,0,0
-#define RC_PRODUCTVERSION2 "3.1.0"
+#define RC_FILEVERSION 3,2,0,0
+#define RC_PRODUCTVERSION 3,2,0,0
+#define RC_PRODUCTVERSION2 "3.2.0"
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
